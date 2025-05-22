41887afe9f7c82514091fb1bf34c168c0ca170f9
Truong Giang <giang11a02@example.com>
2025-05-22 21:34:55 +0700

    [add] lab-01 ex01 ex02]

---
+++ lab-01/ex02/ex02_03.py
@@ -0,0 +1,7 @@
+#Nhập số từ người dùng
+so = input("Nhâoj một số nguyên:")
+#Kiểm tra xem số đó có phải số chẵn không
+if so % 2 == 0:
+    print(so, "là số chẵn.")
+else:
+    print(so, "là số lẻ")
